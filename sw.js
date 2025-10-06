6e647d6433863822e1ecd48dffd19a11290b1319
Luis Enrique Sanchez Romero <luiskike2210@gmail.com>
2025-10-06 10:39:17 -0600

    first commit

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Basic structure of a service worker file (sw.js)
+// 1. Name the cache and files to cache 
+const CACHE_NAME = ' my-site-cache-v1';
+const urlsToCache = [
+    "index.html",
+    // "styles.css",
+    // "app.js",
+    "offline.html"
+];
+
+//2. Install -> ejectuted when the service worker is intalled
+self.addEventListener("install", event =>{
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache=>{
+            cache.addAll(urlsToCache)
+        })
+    )
+})
+//3. Activate -> ejecuted the service worker is activated(clean old caches)
+self.addEventListener("activate", event=>{
+    event.waitUntil(
+        caches.keys().then(keys=>{
+            Promise.all(
+                keys.filter(key=>key !== CACHE_NAME).map(key =>caches.delete(key))
+            )
+        })
+    )
+})
